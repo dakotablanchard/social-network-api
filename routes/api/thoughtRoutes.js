d867709579cf65728e101518836f9344377e0b1f
Dakota Blanchard <dakotablanchard2699@gmail.com>
2024-01-31 20:02:08 -0500

    updated userRoutes and thoughtRoutes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,7 +1,34 @@
 const router = require('express').Router();
 
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addComment,
+    updateComment,
+    deleteComment
+} = require('../../controllers/thought-controller');
 
+// /api/thoughts
+// Gett all thoughts or create a new thought
+router.route('/')
+    .get(getAllThoughts)
+    .post(createThought);
 
+// /api/thoughts/:thoughtId
+// Get a single thought, update a thought, or delete a thought
+router.route('/:thoughtId')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
 
+// /api/thoughts/comments/:thoughtId
+// Add a comment to a thought, update a comment, or delete a comment
+router.route('/comments/:thoughtId/')
+    .post(addComment)
+    .put(updateComment)
+    .delete(deleteComment);
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
 
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+} = require('../../controllers/userController.js');
 
+// /api/users
+// Get all users or create a new user
+router.route('/')
+    .get(getUsers)
+    .post(createUser);
 
-
+// /api/users/:userId
+// Get a single user, update a user, or delete a user
+router.route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
 
 module.exports = router;
