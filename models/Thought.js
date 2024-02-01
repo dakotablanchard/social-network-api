1e35a30128bb18d9fe7e9c4b622f5cd3128f246f
Dakota Blanchard <dakotablanchard2699@gmail.com>
2024-01-31 19:09:31 -0500

    Created Thought model

---
+++ models/Thought.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min_length: 1,
+            max_length: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Comment'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    });
+
+    const Thought = model('Thought', thoughtSchema);
+
+    module.exports = Thought;
